42ee51cf65ec4f63206b98a05453d9f3e196ab2b
Andrew Chan <chan.andrew1995@gmail.com>
2024-11-18 20:39:40 -0500

    Created the frontend project and added in tanstackquery and phaser

---
+++ jp-word-quest-front/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Main from './components/Main/Main';
+import './App.css'
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Main />
+    </QueryClientProvider>
+  )
+}
+
+export default App
---
+++ jp-word-quest-front/src/components/Main/Main.tsx
@@ -0,0 +1,9 @@
+const Main = (): JSX.Element => {
+    return (
+        <>
+            This is main
+        </>
+    )
+}
+
+export default Main;
